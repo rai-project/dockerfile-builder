1d1e92ba006438e1fdf02a4f5d9f1922a3efd996
Abdul Dakkak <abduld@wolfram.com>
2017-05-03 22:57:57 -0500

    change name

--- src/modules/app/index.js
+++ src/modules/app/index.js
@@ -2,7 +2,7 @@ import codeEditorButtonClicked from './signals/codeEditorButtonClicked';
 
 export default {
 	state: {
-		name: 'RAI Dockerfile Builder',
+		name: 'Dockerfile Builder for Power',
 		isLoading: true,
 		isSaving: false,
 		isBuilding: false,
