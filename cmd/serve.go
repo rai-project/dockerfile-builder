4ce55e999d714e95b82c592d455ca4ec1c316959
Abdul Dakkak <abduld@wolfram.com>
2017-05-30 12:56:18 -0400

    read the port env var

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,22 +1,27 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/rai-project/dockerfile-builder/server"
 	"github.com/spf13/cobra"
+)
+
+const (
+	DEFAULTPORT = "8088"
 )
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Start the webserver",
 	Run: func(cmd *cobra.Command, args []string) {
-		server.Start(":8088")
+		port, found := os.LookupEnv("PORT")
+		if !found {
+			port = DEFAULTPORT
+		}
+		server.Start(fmt.Sprintf(":%s", port))
 	},
 }
 
