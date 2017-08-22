7e82f5ccba658b7176aa0c46444ccd35019ca708
Abdul Dakkak <abduld@wolfram.com>
2017-08-22 01:44:38 +0100

    check for dockerfile

---
+++ src/modules/app/actions/checkForDockerfile.js
@@ -0,0 +1,14 @@
+import { isNil, has } from "lodash";
+
+export default function checkForDockerfile({ path, props: { content } }) {
+  if (has(content, "Dockerfile")) {
+    return path.success();
+  }
+
+  return path.error({
+    error: {
+      message: "no dockerfile found",
+      stack: []
+    }
+  });
+}
--- src/modules/app/signals/fileUploaded.js
+++ src/modules/app/signals/fileUploaded.js
@@ -3,6 +3,7 @@ import { state, props } from "cerebral/tags";
 
 import fromZipFile from "../actions/fromZipFile";
 import removeZipRoot from "../actions/removeZipRoot";
+import checkForDockerfile from "../actions/checkForDockerfile";
 
 import onError from "../../common/chains/onError";
 import resetError from "../../common/chains/resetError";
@@ -17,8 +18,14 @@ export default [
     success: [
       set(state`app.upload.files`, props`content`),
       removeZipRoot,
-      set(state`app.files.content`, props`content`),
-      set(state`app.files.prefix`, props`prefix`)
+      checkForDockerfile,
+      {
+        success: [
+          set(state`app.files.content`, props`content`),
+          set(state`app.files.prefix`, props`prefix`)
+        ],
+        error: onError
+      }
     ],
     error: onError
   },
