d3b3767bbc6e5f1b333e0de675378e2328125301
Abdul Dakkak <abduld@wolfram.com>
2018-10-25 14:54:24 -0500

    get resources

--- resources.go
+++ resources.go
@@ -1,3 +1,4 @@
+//go:generate go get github.com/mjibson/esc
 //go:generate esc -o cmd/static_content.go -pkg cmd -private ./.dockerfile_builder_config.yml
 
 //DISABLED go:generate esc -o server/static_content.go -pkg server -private ./server/rai_build.template
