910192ba758364d74aa4975e2d3aea50dd758a75
Abdul Dakkak <abduld@wolfram.com>
2019-08-02 12:35:06 -0500

    fixes

--- src/components/Pages/Images.js
+++ src/components/Pages/Images.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { head, split, flatten } from "lodash";
+import { head, split, flatten, isNil } from "lodash";
 import { Table, Icon } from "semantic-ui-react";
 import Markdown from "../Markdown";
 
 import imagesJSON from "../../assets/Dockerfiles-ppc64le.json";
 
-const REPO_PREFIX =
-  "https://github.com/rai-project/Dockerfiles-ppc64le/blob/master/";
-const RAW_REPO_PREFIX =
-  "https://raw.githubusercontent.com/rai-project/Dockerfiles-ppc64le/master/";
+const REPO_PREFIX = "https://github.com/rai-project/Dockerfiles-ppc64le/blob/master/";
+const RAW_REPO_PREFIX = "https://raw.githubusercontent.com/rai-project/Dockerfiles-ppc64le/master/";
 
 export default class Images extends React.Component {
   state = { activeId: 0 };
@@ -20,40 +18,41 @@ export default class Images extends React.Component {
   render() {
     const { activeId } = this.state;
     const urlOf = name => "https://hub.docker.com/r/" + head(split(name, ":"));
-    const rows = imagesJSON.map(
-      ({ id, name, dockerfile, readme, architecture }) => {
-        const readmeRawURL = readme.replace(REPO_PREFIX, RAW_REPO_PREFIX);
-        const iconName = activeId === id ? "dropdown" : "triangle right";
-        return [
-          <Table.Row key={id} active={activeId === id}>
-            <Table.Cell singleLine textAlign="left">
-              <Icon name={iconName} onClick={() => this.handleClick(id)} />
-              <a href={urlOf(name)}>{name}</a>
-            </Table.Cell>
-            <Table.Cell textAlign="center">
-              <a href={dockerfile}>
-                <Icon name="file text outline" />
-              </a>
-            </Table.Cell>
-            <Table.Cell textAlign="center">
-              <a href={readmeRawURL}>
-                <Icon name="file text outline" />
-              </a>
-            </Table.Cell>
-            <Table.Cell singleLine textAlign="left">
-              {architecture}
-            </Table.Cell>
-          </Table.Row>,
-          activeId === id ? (
-            <Table.Row key={id + "README"}>
-              <Table.Cell colSpan="4">
-                <Markdown url={readmeRawURL} />
-              </Table.Cell>
-            </Table.Row>
-          ) : null
-        ];
+    const rows = imagesJSON.map(({ id, name, dockerfile, readme, architecture }) => {
+      if (isNil(id)) {
+        id = name;
       }
-    );
+      const readmeRawURL = readme.replace(REPO_PREFIX, RAW_REPO_PREFIX);
+      const iconName = activeId === id ? "dropdown" : "triangle right";
+      return [
+        <Table.Row key={id} active={activeId === id}>
+          <Table.Cell singleLine textAlign="left">
+            <Icon name={iconName} onClick={() => this.handleClick(id)} />
+            <a href={urlOf(name)}>{name}</a>
+          </Table.Cell>
+          <Table.Cell textAlign="center">
+            <a href={dockerfile}>
+              <Icon name="file text outline" />
+            </a>
+          </Table.Cell>
+          <Table.Cell textAlign="center">
+            <a href={readmeRawURL}>
+              <Icon name="file text outline" />
+            </a>
+          </Table.Cell>
+          <Table.Cell singleLine textAlign="left">
+            {architecture}
+          </Table.Cell>
+        </Table.Row>,
+        activeId === id ? (
+          <Table.Row key={id + "README"}>
+            <Table.Cell colSpan="4">
+              <Markdown url={readmeRawURL} />
+            </Table.Cell>
+          </Table.Row>
+        ) : null
+      ];
+    });
     return (
       <Table celled striped selectable padded>
         <Table.Header>
